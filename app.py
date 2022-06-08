841588c786b36ef8c9cdcefdf0f6d3a26ae2bf30
erick-alvarado <alexanderguerra212@gmail.com>
2022-06-08 00:20:16 -0600

    Se agrego informacion del developer 201800546
    Añade info de Erick Alvarado

---
+++ app.py
@@ -0,0 +1,18 @@
+import json
+from flask import Flask, jsonify, request
+from flask_cors import CORS, cross_origin
+
+app = Flask(__name__)
+CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
+
+
+
+@app.route("/")
+@cross_origin()
+def hello():
+    return jsonify({"name":"Analisi y Diseño 1",
+                    "estudiante1":"Erick Alexander Alvarado Guerra - 201800546 - Practica 1 - Grupo 2"})
+
+if __name__ == '__main__':
+    app.run(port=3000, debug=True)
